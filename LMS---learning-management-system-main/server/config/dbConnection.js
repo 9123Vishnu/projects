87810f6ebee389a3fabf5a15edd7ed7db42947f6
9123Vishnu <142998685+9123Vishnu@users.noreply.github.com>
2023-11-23 10:48:08 +0530

    Update dbConnection.js

--- LMS---learning-management-system-main/server/config/dbConnection.js
+++ LMS---learning-management-system-main/server/config/dbConnection.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 mongoose.set('strictQuery', false);
+const MONGO_URL = "mongodb+srv://vishnuvardhan21bce9004:gANESH1121@cluster0.lxhgfpf.mongodb.net/?retryWrites=true&w=majority"
 
 const connectionToDB = async () => {
     try {
-        const { connection } = await mongoose.connect(process.env.MONGO_URL);
+        const { connection } = await mongoose.connect(MONGO_URL);
         if (connection) {
             console.log(`Connected to MongoDB: ${connection.host}:${connection.port}`);
         }
